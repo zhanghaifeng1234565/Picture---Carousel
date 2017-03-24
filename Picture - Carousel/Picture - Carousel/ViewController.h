33862b817352bac46abec972abceab6dad3bcb38
zhanghaifeng <18235148520@163.com>
2017-03-25 07:53:16 +0800

    创建项目

---
+++ Picture - Carousel/Picture - Carousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Picture - Carousel
+//
+//  Created by zhanghaifeng on 2017/3/25.
+//  Copyright © 2017年 BBSWaitting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Picture - Carousel/Picture - CarouselTests/Picture___CarouselTests.m
@@ -0,0 +1,39 @@
+//
+//  Picture___CarouselTests.m
+//  Picture - CarouselTests
+//
+//  Created by zhanghaifeng on 2017/3/25.
+//  Copyright © 2017年 BBSWaitting. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Picture___CarouselTests : XCTestCase
+
+@end
+
+@implementation Picture___CarouselTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
